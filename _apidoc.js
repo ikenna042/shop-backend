dbfe2e764b403a16312525280a2456b04c910f62
IkennaVictor <ikennavictorofficial@gmail.com>
2019-09-16 19:07:00 +0100

    added apiDoc and doc version

--- _apidoc.js
+++ _apidoc.js
@@ -1,8 +1,3 @@
-const express = require('express');
-const router = express.Router();
-
-const userCtrl = require('../controllers/user');
-
 /**
  * @apiDefine UserError
  * @apiError {Object} 400 Some parameters may contain invalid values.
@@ -12,24 +7,21 @@ const userCtrl = require('../controllers/user');
 
 /**
  * @api {post} /api/stuff Create user
- * @apiVersion 0.2.0
+ * @apiVersion 0.1.0
  * @apiName CreateUser
  * @apiGroup User
  * @apiHeader {String} Authorization Bearer token
  * @apiParam {String} [email] Email of the user (required)
  * @apiParam {String} [password] Password of the user (required)
- * @apiSuccess {String} email Email of the User
  * @apiSuccessExample Success-Response:
  *      HTTP/1.1 201 OK
  * 
  * @apiUse UserError
- * @apiDescription This holds all record of users.
  */
-router.post('/signup', userCtrl.signup);
 
 /**
  * @api {post} /api/stuff Create user
- * @apiVersion 0.2.0
+ * @apiVersion 0.1.0
  * @apiName LoginUser
  * @apiGroup User
  * @apiHeader {String} Authorization Bearer token
@@ -40,20 +32,3 @@ router.post('/signup', userCtrl.signup);
  * 
  * @apiUse UserError
  */
-router.post('/login', userCtrl.login);
-
-/**
- * @api {post} /api/stuff Create example
- * @apiVersion 0.2.0
- * @apiName LoginExample
- * @apiGroup Example
- * @apiHeader {String} Authorization Bearer token
- * @apiParam {String} [email] Email of the example (required)
- * @apiParam {String} [password] Password of the example (required)
- * @apiSuccessExample Success-Response:
- *      HTTP/1.1 201 OK
- * 
- * @apiUse UserError
- */
-
-module.exports = router;
--- models/user.js
+++ models/user.js
@@ -1,3 +1,12 @@
+/* eslint-disable import/no-cycle */
+/**
+ * @author 4Decoder
+ * @property {ObjectId} id User ObjectId primaryKey
+ * @property {String} email User email (required)
+ * @property {String} password User password (required)
+ * @description User holds record of all users in the store.
+ */
+
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
